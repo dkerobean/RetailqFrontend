7c31e18c7f0e0e785b905ef9276289a7b58aaf1e
Dickson <dkerobean@gmail.com>
2024-01-29 21:38:12 +0000

    bug fix for account

--- src/layouts/full/horizontal/header/Header.js
+++ src/layouts/full/horizontal/header/Header.js
@@ -66,11 +66,11 @@ const Header = () => {
         ) : null}
         <Box flexGrow={1} />
         <Stack spacing={1} direction="row" alignItems="center">
-          <Language />
+          {/* <Language /> */}
           {/* ------------------------------------------- */}
           {/* Ecommerce Dropdown */}
           {/* ------------------------------------------- */}
-          <Cart />
+          {/* <Cart /> */}
           {/* ------------------------------------------- */}
           {/* End Ecommerce Dropdown */}
           {/* ------------------------------------------- */}
